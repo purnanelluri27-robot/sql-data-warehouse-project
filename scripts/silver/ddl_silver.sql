c30f569d806e137454e7d1f4bfff522152482974
WALTER <purnanelluri27@gmail.com>
2026-02-27 00:32:20 -0600

    Add GO statements for SQL command separation
    Added GO statements to separate SQL commands and ensure proper execution.

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -7,7 +7,7 @@ Run this script to redefine the ddl structure of silver table.
 
 IF OBJECT_ID ('silver.crm_cust_info','U') IS NOT NULL
     DROP TABLE silver.crm_cust_info;
-
+GO
 
 CREATE TABLE silver.crm_cust_info(
 cst_id INT,
@@ -19,12 +19,13 @@ cst_gndr NVARCHAR(50),
 cst_create_date DATE 
 );
 
-
+GO
 
 IF OBJECT_ID ('silver.crm_prd_info','U') IS NOT NULL
     DROP TABLE silver.crm_prd_info;
 
-
+GO
+    
 CREATE TABLE silver.crm_prd_info(
 prd_id INT,
 cat_id NVARCHAR(50),
@@ -37,11 +38,11 @@ prd_end_dt DATE
 
 );
 
-
+GO
 
 IF OBJECT_ID ('silver.crm_sales_details','U') IS NOT NULL
     DROP TABLE silver.crm_sales_details;
-
+GO
 
 CREATE TABLE silver.crm_sales_details(
 sls_ord_num NVARCHAR(50),
@@ -55,33 +56,38 @@ sls_quantity INT,
 sls_price INT
 );
 
-
+GO
 
 IF OBJECT_ID ('silver.erp_cust_az12','U') IS NOT NULL
     DROP TABLE silver.erp_cust_az12;
 
-
+GO
+    
 CREATE TABLE silver.erp_cust_az12(
 cid NVARCHAR(50),
 bdate DATE,
 gen NVARCHAR(50)
 );
 
-
+GO
 
 IF OBJECT_ID ('silver.erp_loc_a101','U') IS NOT NULL
     DROP TABLE silver.erp_loc_a101;
 
+GO
+    
 CREATE TABLE silver.erp_loc_a101(
 cid NVARCHAR(50),
 cntry NVARCHAR(50)
 );
 
-
+GO
 
 IF OBJECT_ID ('silver.erp_px_cat_g1v2','U') IS NOT NULL
     DROP TABLE silver.erp_px_cat_g1v2;
 
+GO
+    
 CREATE TABLE silver.erp_px_cat_g1v2(
 id NVARCHAR(50),
 cat NVARCHAR(50),
@@ -89,3 +95,4 @@ subcat NVARCHAR(50),
 maintenance NVARCHAR(50)
 );
 
+GO
